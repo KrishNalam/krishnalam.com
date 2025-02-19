59d0643da6ad19caac07a4d29cf0fb729899529c
KoolKrish18 <nalamkrish1@gmail.com>
2025-02-18 20:12:06 -0500

    add better meta tag desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,52 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import { ThemeProvider } from "./provider";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+import { ThemeProvider } from './provider';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+	src: './fonts/GeistVF.woff',
+	variable: '--font-geist-sans',
+	weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+	src: './fonts/GeistMonoVF.woff',
+	variable: '--font-geist-mono',
+	weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Krish's Portfolio",
-  description: "Modern & Minimal Portfolio",
+	title: "Krish's Portfolio",
+	description: 'Hey, this is Krish Nalam, a full-stack developer currently studying at UofT!',
+	keywords: [
+		'Krish',
+		'Nalam',
+		'Krish Nalam',
+		'Portfolio',
+		'Developer',
+		'software engineer',
+		'web developer',
+		'projects',
+		'extensions',
+		'chrome extensions',
+		'python',
+		'javascript',
+		'react',
+	],
+	creator: 'Krish Nalam',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				<ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
